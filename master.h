35108f125600b4f15b1dadca78839758da19859d
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-04 21:14:32 +0800

    Update master.h

---
+++ master.h
@@ -0,0 +1,16 @@
+#ifndef MASTER_H
+#define MASTER_H
+
+#define VERSION "1.0.0"
+#define DEBUG true
+
+/**
+ * File ini berisi library yang digunakan untuk berkomunikasi antara robot 1 dan robot 2 melalui bluetooth
+ * File ini akan di include pada file utama yang akan dipanggil
+ * oleh fungsi main(), sehingga file ini akan di compile bersamaan dengan file utama
+ * menggunakan modul bluetooth HC-05,dimana file akan dimasukan pada robot 1 yang berfungsi sebagai master 
+ * komunikasi antara robot 1 dan robot 2
+ * 
+ */
+
+#endif
