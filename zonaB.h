0d8e0f3c65e273a1af0e0ee0d8b9cb96c1dc83fa
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-04 21:14:04 +0800

    Update zonaB.h

---
+++ zonaB.h
@@ -0,0 +1,11 @@
+#ifndef ZONA_B_H
+
+#define VERSION "1.0.0"
+#define DEBUG true
+
+/**
+ * zonaA berisi sekumpulan fungsi dan sekuensial timing yang akan memanggil 
+ * member servo ID dan menuliskan derajat servo dan waktu gerakan tarian pada zona B.
+ */
+#endif
+
