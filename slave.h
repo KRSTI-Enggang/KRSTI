ccf04c9b9e95b76066f9a3f477a44a8b91fc12a2
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-04 21:14:25 +0800

    Update slave.h

---
+++ slave.h
@@ -0,0 +1,16 @@
+#ifndef SLAVE_H
+#define SLAVE_H
+
+#define VERSION "1.0.0"
+#define DEBUG true
+
+/**
+ * File ini berisi library yang digunakan untuk berkomunikasi antara robot 1 dan robot 2 melalui bluetooth
+ * File ini akan di include pada file utama yang akan dipanggil
+ * oleh fungsi main(), sehingga file ini akan di compile bersamaan dengan file utama
+ * menggunakan modul bluetooth HC-05,dimana file akan dimasukan pada robot 2 yang berfungsi sebagai slave 
+ * yang meenrima komunikasi antara robot 1 dan robot 2, yang dimana hanya menerima data dari robot 1
+ * 
+ */
+
+#endif
