d5c896a309fb4fb25f51bc7dd0fab80623a33313
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-04 21:12:28 +0800

    Update klasifikasiSuara.h

---
+++ klasifikasiSuara.h
@@ -0,0 +1,14 @@
+#ifndef KLASIFIKASI_SUARA_H
+#define KLASIFIKASI_SUARA_H
+
+/*
+* File ini berisi library yang digunakan untuk mengklasifikasikan suara yang didengar oleh robot
+* fungsi dari file ini akan di pada file utama robot 1 untuk menjadi flag gerakan mulai atau berhenti
+* ketika menerima lagu yang akan diputar
+* yang dimana,file ini akan menerima inputan dari
+*/
+
+#define VERSION "1.0.0"
+#define DEBUG true
+
+#endif
