2c9c15229c966c094f927c698e6479ee2bfab13d
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-04 21:14:19 +0800

    Update tubuhKiri.h

---
+++ tubuhKiri.h
@@ -0,0 +1,16 @@
+#ifndef TUBUHKIRI_H
+#define TUBUHKIRI_H
+
+#define VERSION "1.0.0"
+#define DEBUG true
+
+/**
+ * File yang berisi sekumpulan member untuk menggambar tubuh tengah
+ * menggunakan Servo Ax-12A ( Dynamixel) 
+ * atau servo arduino compatible   
+ * yang dimana file ini akan di include pada file utama yang akan dipanggil
+ * oleh fungsi main(), sehingga file ini akan di compile bersamaan dengan file utama
+ * file ini berisi ID servo pada tiap-tiap servo yang akan digunakan pada sendi tubuh
+ */
+
+#endif
