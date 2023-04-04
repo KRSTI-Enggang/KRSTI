b7bbcf29432cbac81c0ee92c936518c5fe965419
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-04 21:14:14 +0800

    Update zonaA.h

---
+++ zonaA.h
@@ -0,0 +1,13 @@
+#ifndef ZONA_A_H
+#define ZONA_A_H
+
+#define VERSION "1.0.0"
+#define DEBUG true
+
+/**
+ * zonaA berisi sekumpulan fungsi dan sekuensial timing yang akan memanggil 
+ * member servo ID dan menuliskan derajat servo dan waktu gerakan pembuka pada zona A.
+ */
+
+#endif
+
