522991e282015077776c3f8faefc0bd623af9617
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-04 21:12:59 +0800

    Initial Commit

---
+++ zonaC.h
@@ -0,0 +1,13 @@
+#ifndef ZONA_C_H
+#define ZONA_C_H
+
+#define VERSION "1.0.0"
+#define DEBUG true
+
+/**
+ * zonaA berisi sekumpulan fungsi dan sekuensial timing yang akan memanggil 
+ * member servo ID dan menuliskan derajat servo dan waktu gerakan penutup pada zona C.
+ */
+
+#endif
+
