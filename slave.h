a2f63828e445751c9d65bc2bc253c4fd0a58eb6e
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-07 20:53:51 +0800

    Update slave.h

--- slave.h
+++ slave.h
@@ -1,9 +1,10 @@
+#include "WString.h"
 #ifndef SLAVE_H
 #define SLAVE_H
-
 #define VERSION "1.0.0"
 #define DEBUG true
-
+#include <Arduino.h>
+int statusSlave;
 /**
  * File ini berisi library yang digunakan untuk berkomunikasi antara robot 1 dan robot 2 melalui bluetooth
  * File ini akan di include pada file utama yang akan dipanggil
@@ -11,5 +12,12 @@
  * ESP32 menerima dari master dan akan dikirimkan ke arduino melalui serial,
  * 
  */
+void slave(){
+    if (Serial.available()) {
+    statusSlave = Serial.read();
+    Serial.print(" flag yang diterima: ");
+    Serial.println(statusSlave);
+  }
 
+}
 #endif
