77a2d39b27202ecd06addcd280fea4cfc9cb8604
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-07 08:13:01 +0800

    Update klasifikasiSuara.h

--- klasifikasiSuara.h
+++ klasifikasiSuara.h
@@ -1,48 +1,32 @@
-#ifndef KLASIFIKASI_SUARA_H
-#define KLASIFIKASI_SUARA_H
-
+#ifndef KLASIFIKASISUARA_H
+#define KLASIFIKASISUARA_H
 #define VERSION "1.00"
 #define DEBUG true.
 #include <Arduino.h>
 
 /*
-* File ini berisi library yang digunakan untuk mengklasifikasikan suara yang didengar oleh robot
-* fungsi dari file ini akan di pada file utama robot 1 untuk menjadi flag gerakan mulai atau berhenti
-* ketika menerima lagu yang akan diputar
-* yang dimana,file ini akan menerima inputan dari
+  File ini berisi library yang digunakan untuk mengklasifikasikan suara yang didengar oleh robot
+  fungsi dari file ini akan di pada file utama robot 1 untuk menjadi flag gerakan mulai atau berhenti
+  ketika menerima lagu yang akan diputar
+  yang dimana,file ini akan menerima inputan dari
 */
 const uint8_t pinSuara = A0;
 int nilai;
 float tegangan;
-bool musik;
+bool musik = false;
 
 void suaraMusik() {
-
-    nilai = analogRead(pinSuara);
-    tegangan = nilai * 5.0 / 1023.0;
-
-
-
-
+  nilai = analogRead(pinSuara);
+  tegangan = nilai * 5.0 / 1023.0;
 }
 
 
 void klasifikasiSuara() {
-    if (tegangan < 5) {
-        if (musik == HIGH) {
-
-        } else {
-
-        }
-    } else {
-        if(musik == HIGH) {
-
-        } else {
-
-        }
-    }
-
-
-
-
+  if (tegangan <= 3) {
+    musik = false;
+  }
+  else if (tegangan >= 3) {
+    musik = true;
+  }
+}
 #endif
