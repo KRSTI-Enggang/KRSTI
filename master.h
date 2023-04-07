560078f9bb28e07d0e173692727b05830ce8531d
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-07 20:53:47 +0800

    Update master.h

--- master.h
+++ master.h
@@ -1,8 +1,10 @@
 #ifndef MASTER_H
 #define MASTER_H
-
 #define VERSION "1.0.0"
 #define DEBUG true
+#include <Arduino.h>
+#include "klasifikasiSuara.h"
+int statusMaster;
 
 /**
  * File ini berisi library yang digunakan untuk berkomunikasi antara robot 1 dan robot 2 melalui bluetooth
@@ -11,5 +13,9 @@
  * menggunakan bluetooth ESP32,arduino akan mengirimkan ke ESP32 melalui serial,dan akan diteruskan ke ESP32 sebagai master ke robot 2
  * 
  */
+
+void master() {
+  statusMaster = Serial.write(musik);
+}
 
 #endif
