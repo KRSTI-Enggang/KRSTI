08b2ca2cc80ac0ed1cd5dcbbfabe67a4d6e8cfae
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-07 11:35:18 +0800

    Defining Servo Pins

--- tubuhKanan.h
+++ tubuhKanan.h
@@ -1,8 +1,9 @@
 #ifndef TUBUHKANAN_H
 #define TUBUHKANAN_H
-
 #define VERSION "1.0.0"
 #define DEBUG true
+#include <Servo.h>
+Servo servoKanan;
 
 /**
  * File yang berisi sekumpulan member untuk menggambar tubuh kanan
@@ -12,6 +13,32 @@
  * oleh fungsi main(), sehingga file ini akan di compile bersamaan dengan file utama
  * file ini berisi ID servo pada tiap-tiap servo yang akan digunakan pada sendi tubuh
  */
+
+int pergelanganTanganKananY = 4;
+int lenganKananY = 5;
+int lenganKananX = 6;
+int bahuKananY = 7;
+int bahuKananX = 8;
+int pahaKananY = 9;
+int lututKananY = 10;
+int pergelanganKakikananY = 11;
+int pergelanganKakikananX = 12;
+int pinggulkananY = 13;
+int pinggulkananX = 14;
+
+void tubuhKanan() {
+  servoKanan.attach(pergelanganTanganKananY);
+  servoKanan.attach(lenganKananY);
+  servoKanan.attach(lenganKananX);
+  servoKanan.attach(bahuKananY);
+  servoKanan.attach(bahuKananX);
+  servoKanan.attach(pahaKananY);
+  servoKanan.attach(lututKananY);
+  servoKanan.attach(pergelanganKakikananY);
+  servoKanan.attach(pergelanganKakikananX);
+  servoKanan.attach(pinggulkananY);
+  servoKanan.attach(pinggulkananX);
+}
 
 
 #endif
