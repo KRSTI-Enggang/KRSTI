f312db79866b492378d85be32a6a9c491fe3ee0c
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-07 11:35:27 +0800

    Defining Servo Pins

--- tubuhTengah.h
+++ tubuhTengah.h
@@ -1,8 +1,9 @@
 #ifndef TUBUHTENGAH_H
 #define TUBUHTENGAH_H
-
 #define VERSION "1.0.0"
 #define DEBUG true
+#include <Servo.h>
+Servo servo;  
 
 /**
  * File yang berisi sekumpulan member untuk menggambar tubuh tengah
@@ -12,5 +13,16 @@
  * oleh fungsi main(), sehingga file ini akan di compile bersamaan dengan file utama
  * file ini berisi ID servo pada tiap-tiap servo yang akan digunakan pada sendi tubuh
  */
+
+int kepalaX = 1;
+int kepalaY = 2;
+int pinggangX = 3;
+
+
+void tubuhTengah() {
+  servo.attach(kepalaX);
+  servo.attach(kepalaY);
+  servo.attach(pinggangX);
+}
 
 #endif
