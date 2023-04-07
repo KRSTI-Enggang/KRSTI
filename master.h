74cefc0cabd2c1da3230e5ec98de7059754b598c
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-07 10:06:10 +0800

    Update master.h

--- master.h
+++ master.h
@@ -8,8 +8,7 @@
  * File ini berisi library yang digunakan untuk berkomunikasi antara robot 1 dan robot 2 melalui bluetooth
  * File ini akan di include pada file utama yang akan dipanggil
  * oleh fungsi main(), sehingga file ini akan di compile bersamaan dengan file utama
- * menggunakan modul bluetooth HC-05,dimana file akan dimasukan pada robot 1 yang berfungsi sebagai master 
- * komunikasi antara robot 1 dan robot 2
+ * menggunakan bluetooth ESP32,arduino akan mengirimkan ke ESP32 melalui serial,dan akan diteruskan ke ESP32 sebagai master ke robot 2
  * 
  */
 
