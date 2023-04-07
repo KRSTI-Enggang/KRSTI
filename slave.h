18603db58992a1a0770c550699c1d182b03b65cc
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-07 10:06:14 +0800

    Update slave.h

--- slave.h
+++ slave.h
@@ -8,8 +8,7 @@
  * File ini berisi library yang digunakan untuk berkomunikasi antara robot 1 dan robot 2 melalui bluetooth
  * File ini akan di include pada file utama yang akan dipanggil
  * oleh fungsi main(), sehingga file ini akan di compile bersamaan dengan file utama
- * menggunakan modul bluetooth HC-05,dimana file akan dimasukan pada robot 2 yang berfungsi sebagai slave 
- * yang meenrima komunikasi antara robot 1 dan robot 2, yang dimana hanya menerima data dari robot 1
+ * ESP32 menerima dari master dan akan dikirimkan ke arduino melalui serial,
  * 
  */
 
